ce3ed41f826ceced87c0d7e3db94eaaaace60310
guilhermeapgap <93015889+guilhermeapgap@users.noreply.github.com>
2023-04-16 21:15:49 -0300

    modelo

--- src/components/bootstrap/carousel/index.js
+++ src/components/bootstrap/carousel/index.js
@@ -3,7 +3,7 @@ import style from './carousel.module.scss'
 
 
 //img
-import foto1 from '../../../img/gui1.jpg'
+import modelo1 from '../../../img/modelo1'
 import foto2 from '../../../img/vini1.jpg'
 import foto3 from '../../../img/gui3.jpg'
 
@@ -34,7 +34,7 @@ function Carousel1() {
         
       </Carousel.Item>
       <Carousel.Item className={style.box_img}>
-        <img src={foto3} alt='foto slide'/>
+        <img src={modelo1} alt='foto slide'/>
         <img src={foto2} alt='foto slide' className={style.img_carousel_responsive}/>
 
         <Carousel.Caption>
